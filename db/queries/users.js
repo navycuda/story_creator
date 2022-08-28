02546a18b61a93fc8884f15aed07800b208aefc0
NavyCuda <navysupra@gmail.com>
2022-08-28 12:54:07 -0700

    Change query to pool

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,7 +1,7 @@
 const db = require('../connection');
 
 const getUsers = () => {
-  return db.query('SELECT * FROM users;')
+  return db.pool('SELECT * FROM users;')
     .then(data => {
       return data.rows;
     });
