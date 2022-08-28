9fc013fcc605e6426970c3b5ba8860d7ac331bde
NavyCuda <navysupra@gmail.com>
2022-08-28 00:01:03 -0700

    Complete stories schema

--- db/schema/03_stories.sql
+++ db/schema/03_stories.sql
@@ -1,3 +1,18 @@
 /*
 \i db/schema/03_stories.sql
 */
+DROP TABLE IF EXISTS
+  stories CASCADE
+;
+CREATE TABLE
+  stories (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    owner_id INTEGER REFERENCES
+      users(id)
+      ON DELETE CASCADE,
+      created_at TIMESTAMP,
+      started_at TIMESTAMP,
+      completed_at TIMESTAMP
+  )
+;
