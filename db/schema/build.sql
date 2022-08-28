c955c595e48f262dc678d0597cdb139e45da801a
NavyCuda <navysupra@gmail.com>
2022-08-28 10:59:17 -0700

    Add table references to build

--- db/schema/build.sql
+++ db/schema/build.sql
@@ -23,6 +23,10 @@ GRANT ALL PRIVILEGES ON DATABASE
   story_creator
 ;
 \c story_creator story_creator;
--- \i db/schema/01_users.sql;
-
--- \i db/schema/01_users.sql;
+\i db/schema/01_users.sql;
+\i db/schema/02_stories.sql;
+\i db/schema/03_contributors.sql;
+\i db/schema/04_characters.sql;
+\i db/schema/05_event_types.sql;
+\i db/schema/06_events.sql;
+\i db/schema/07_votes.sql;
