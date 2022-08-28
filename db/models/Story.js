a56f4611d360c131506c9ae0812e092e3ef4edd7
NavyCuda <navysupra@gmail.com>
2022-08-27 23:53:30 -0700

    Fill out Story class

--- db/models/Story.js
+++ db/models/Story.js
@@ -1,1 +1,21 @@
 // db/models/Story.js
+
+class Story {
+  // name
+  // contributor_id
+  // owner_id
+  // created_at
+  // started_at
+  // completed_at
+
+  constructor(name, owner_id) {
+    this.name = name;
+    this.owner_id = owner_id;
+
+    const timeStamp = Date.now();
+
+    this.created_at = timeStamp;
+    this.started_at = timeStamp;
+    this.completed_at = timeStamp;
+  }
+}
