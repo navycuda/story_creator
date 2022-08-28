d079233092062334ee2bfa645d1b6ebe1e59e6ac
NavyCuda <navysupra@gmail.com>
2022-08-27 23:12:23 -0700

    Create User class

---
+++ db/models/User.js
@@ -0,0 +1,11 @@
+// name: VARCHAR(255)
+// email: VARCHAR(255)
+// password: VARCHAR(255)
+
+class User {
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}
