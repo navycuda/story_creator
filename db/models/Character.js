a24376f5f7c3aa6b489597b11be4b0a73f629698
NavyCuda <navysupra@gmail.com>
2022-08-28 00:08:26 -0700

    Complete Character model

---
+++ db/models/Character.js
@@ -0,0 +1,11 @@
+// db/models/Character.js
+class Character {
+  // id
+  // story_id
+  // name
+  // details
+  constructor(story_id, name) {
+    this.name = name;
+    this.story_id = story_id;
+  }
+}
