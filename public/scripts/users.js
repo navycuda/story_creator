7657c379776119ccb3f9239c582d0aab3c517c6a
NavyCuda <navysupra@gmail.com>
2022-08-28 13:19:38 -0700

    Start building out getUser

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,17 +1,23 @@
 // Client facing scripts here
 $(() => {
+  // Get the entire users list
   $('#fetch-users').on('click', () => {
     $.ajax({
       method: 'GET',
       url: '/api/users'
     })
-    .done((response) => {
-      const $usersList = $('#users');
-      $usersList.empty();
+      .done((response) => {
+        const $usersList = $('#users');
+        $usersList.empty();
 
-      for(const user of response.users) {
-        $(`<li class="user">`).text(user.name).appendTo($usersList);
-      }
-    });
+        for (const user of response.users) {
+          $(`<li class="user">`).text(user.name).appendTo($usersList);
+        }
+      });
+  });
+  // Get a user by id
+  $('#fetch-user').on('click', () => {
+    const id = $('#user-id').val();
+    console.log(id);
   });
 });
