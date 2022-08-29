19e47dc115b0dd23f60e4deada5e0e29b0b43ba5
NavyCuda <navysupra@gmail.com>
2022-08-28 19:16:49 -0700

    Start combining all the classes into one file

---
+++ db/models/htmlModels.js
@@ -0,0 +1,37 @@
+// db/models/htmlModels.js
+
+// name: VARCHAR(255)
+// email: VARCHAR(255)
+// password: VARCHAR(255)
+class User {
+  // Remember the id, it's issued by the database
+  constructor(db_user) {
+    console.log(`in User constructor :`, db_user);
+    for (const [ key, value ] of Object.entries(db_user)) {
+      this[key] = value;
+    }
+  }
+  getSingleDetail() {
+    return $(`<li class="user">`).text(this.name);
+  }
+  outputToConsole() {
+    console.log(this);
+  }
+  getHtmlDetails() {
+    const result = $('<div>');
+    const name = $('<h2>').text(this.name);
+    const email = $('<span>').text(this.email);
+
+    result.append(name, email);
+
+    return result;
+  }
+}
+
+try {
+  if (module) {
+    module.exports = User;
+  }
+} catch (error) {
+  // Do nothing.  Nothing at all.
+}
