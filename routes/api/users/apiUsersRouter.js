316a76c783f86e041cd1e66511973aa5ecc7aade
NavyCuda <navysupra@gmail.com>
2022-08-31 23:23:14 -0700

    Refactor and cleanup

--- routes/api/users/apiUsersRouter.js
+++ routes/api/users/apiUsersRouter.js
@@ -22,22 +22,15 @@ router.route('/')
       });
   });
 
-
+// Returns the cookie-session user id biography
 router.route('/bio')
   .get(async(request, response) => {
     const templateVars = await setTemplateVars(request);
-
-    console.log("Uhhh?");
-
-
     response.render('./partials/users/_userBio', templateVars);
-
   });
-
+// Returns the bio of this specific user id
 router.route('/bio/:id')
   .get((request, response) => {
-    const templateVars = setTemplateVars(request);
-
     const id = request.params.id;
     userQueries.getUser(Number(id))
       .then((user) => {
@@ -48,13 +41,7 @@ router.route('/bio/:id')
           .status(500)
           .status(errorMsg(error));
       });
-
-
-    response.render('./partials/users/_userBio', templateVars);
-
   });
-
-
 
 router.route('/:id')
   .get((request, response) => {
@@ -69,7 +56,5 @@ router.route('/:id')
           .status(errorMsg(error));
       });
   });
-
-
 
 module.exports = router;
