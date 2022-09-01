4ea183fc5b8abe1a401f1ea15005c8f34a9bd4f3
NavyCuda <navysupra@gmail.com>
2022-08-31 21:29:23 -0700

    Comment out another previous routing

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use('/api', apiRoutes);
 // app.use('/api/users', userApiRoutes);
 // app.use('/api/stories', storiesApiRoutes);
 // app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
+// app.use('/users', usersRoutes);
 
 
 
