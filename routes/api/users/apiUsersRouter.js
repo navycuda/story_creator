95106118042c4a73d8cc89dd46e8011c96a3ad42
NavyCuda <navysupra@gmail.com>
2022-08-31 21:49:30 -0700

    Refactor for rename

--- routes/api/users/apiUsersRouter.js
+++ routes/api/users/apiUsersRouter.js
@@ -1,17 +1,6 @@
-/*
- * All routes for User Data are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /api/users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require('express');
 const router  = express.Router();
-// const userQueries = require('../../../db/queries/users');
-// const {
-//   User,
-//   Story
-// } = require('../../../db/models/htmlModels');
+const userQueries = require('../../../db/queries/userQueries');
 
 const errorMsg = (error) => {
   return {error: error.message};
@@ -21,34 +10,28 @@ console.log('something else happen?');
 
 router.route('/')
   .get((request, response) => {
-    response.render('./partials/_footer');
+    userQueries.getUsers()
+      .then((users) => {
+        response.json({ users });
+      })
+      .catch((error) => {
+        response
+          .status(500)
+          .json(errorMsg(error));
+      });
   });
 
-
-
-// router.get('/', (req, res) => {
-//   userQueries.getUsers()
-//     .then(users => {
-//       res.json({ users });
-//     })
-//     .catch(error => {
-//       res
-//         .status(500)
-//         .json(errorMsg(error));
-//     });
-// });
-// router.get('/:id', (request, response) => {
-//   const id = request.params.id;
-//   console.log(`get api/users/:id`, id);
-//   userQueries.getUser(Number(id))
-//     .then((user) => {
-//       console.log(`get api/users/:id <user>`, user);
-//       response.json({ user });
-//     })
-//     . catch((error) => {
-//       response
-//         .status(500)
-//         .json(errorMsg(error));
-//     });
-// });
+router.route('/:id')
+  .get((request, response) => {
+    const id = request.params.id;
+    userQueries.getUser(Number(id))
+      .then((user) => {
+        response.json({ user });
+      })
+      .catch((error) => {
+        response
+          .status(500)
+          .status(errorMsg(error));
+      });
+  });
 module.exports = router;
