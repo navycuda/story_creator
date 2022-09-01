d20c0d4de72d11452af7cf2c114225459eac4657
NavyCuda <navysupra@gmail.com>
2022-08-31 21:47:26 -0700

    Add route for stories

--- routes/api/apiRouter.js
+++ routes/api/apiRouter.js
@@ -4,7 +4,10 @@ const router  = express.Router();
 
 
 const apiUsersRoutes = require('./users/apiUsersRouter');
+const apiStoriesRoutes = require('./stories/apiStoriesRouter');
+
 router.use('/users', apiUsersRoutes);
+router.use('/stories', apiStoriesRoutes);
 
 console.log("something happened??");
 
