fd9a9210657d8d5548c60887d1f1f3b83930d6df
Kaiya Hedley <keshedley@gmail.com>
2022-09-02 10:03:07 -0600

    .

--- db/seeds/01_users.sql
+++
@@ -1,3 +0,0 @@
--- Users table seeds here (Example)
-INSERT INTO users (name , email , password) VALUES ('Alice' , '123@gmail.com' , 'password');
-INSERT INTO users (name , email, password ) VALUES ('Kira' , 'ej@g.com', 'password');
